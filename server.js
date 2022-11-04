0d35f2b9e46cb18e009a900bae6c586e84461d63
Lily <lily.majani@ynov.com>
2022-11-04 15:57:47 +0100

    deploy conf server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/tp-workshop'));
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+
-        '/dist/tp-workshop/index.html') );});
-app.listen(process.env.PORT || 8080);
+const express = require("express")
+const app = express()
+app.use(express.static("public"))
+app.get("/", function (req, res) {
+    res.send("<h1>TP Workshop</h1>")
+})
+app.listen(process.env.PORT || 3000,
+    () => console.log("Server is running..."));
